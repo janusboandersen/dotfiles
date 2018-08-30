159e820621700688d83fc925e6eb6e68e6d40bb0
Janus Bo Andersen <janus@janusboandersen.dk>
2018-08-30 17:55:12 +0200

    Update Rakefile, alias and export to allow installation on RPM/Yum

--- include/export.sh
+++ include/export.sh
@@ -8,6 +8,16 @@
 export EDITOR=vim
 
 # Exports the LS_COLORS which is used by GNU LS (gls on mac with coreutils)
-eval `gdircolors ${HOME}/.dircolors`
+
+case "$OSTYPE" in
+	  solaris*) echo "SOLARIS" ;;
+	  darwin*)  eval `gdircolors ${HOME}/.dircolors` ;; 
+	  linux*)   eval `dircolors ${HOME}/.dircolors` ;;
+      bsd*)     echo "BSD" ;;
+	  msys*)    echo "WINDOWS" ;;
+      *)        echo "unknown: $OSTYPE" ;;
+esac
+
+# if [["$OSTYPE"="Darwin"]]; then eval `gdircolors ${HOME}/.dircolors`; fi
 
 
